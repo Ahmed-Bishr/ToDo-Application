4a41ef0fdb004ec4900fc84bdd156b405aca6925
Ahmed746996 <70495039+Ahmed746996@users.noreply.github.com>
2024-02-05 02:14:33 +0200

    Add new settingsFragment activity to AndroidManifest.xml.

---
+++ app/src/main/java/com/example/todoapplication/settingsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.todoapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class settingsFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_fragment)
+
+    }
+}
